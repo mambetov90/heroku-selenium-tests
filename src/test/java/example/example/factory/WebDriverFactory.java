a17a30d2d4422d54854b43e29448a04bf213bf20
mambetov90 <47281723+mambetov90@users.noreply.github.com>
2024-03-24 20:35:36 +0200

    Add files via upload

---
+++ src/test/java/example/example/factory/WebDriverFactory.java
@@ -0,0 +1,31 @@
+package example.example.factory;
+
+import example.example.enums.Browser;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class WebDriverFactory {
+
+    public static WebDriver createWebDriver(Browser browser) {
+        WebDriver driver = null;
+        switch (browser) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+        }
+        return driver;
+    }
+}
--- src/test/java/example/example/tests/BaseTest.java
+++ src/test/java/example/example/tests/BaseTest.java
@@ -1,16 +1,17 @@
 package example.example.tests;
 
 import example.example.context.WebDriverContext;
+import example.example.enums.Browser;
 import example.example.factory.PageinstancesFactory;
 
+import example.example.factory.WebDriverFactory;
 import example.example.pages.HerokuLogOutPage;
 import example.example.pages.HerokuLoginPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import lombok.extern.slf4j.Slf4j;
@@ -44,13 +45,27 @@ public class BaseTest {
      */
     @BeforeClass
     protected void setup() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions ops = new ChromeOptions();
-        ops.addArguments("disable-infobars");
-        driver = new ChromeDriver(ops);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebDriverContext.setDriver(driver);
+
+        List<Browser> browsers = Arrays.asList(Browser.CHROME, Browser.EDGE, Browser.FIREFOX);
+        driver = null;
+
+        for (Browser browser : browsers) {
+            try {
+                driver = WebDriverFactory.createWebDriver(browser);
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                WebDriverContext.setDriver(driver);
+                if (driver != null) {
+                    break;
+                }
+            } catch (Exception e) {
+                log.error("Failed to create WebDriver for " + browser + ": " + e.getMessage());
+            }
+        }
+
+        if (driver == null) {
+            throw new RuntimeException("Unable to create WebDriver instance for any browser");
+        }
 
         herokuLoginPage = PageinstancesFactory.getInstance(HerokuLoginPage.class);
         herokuLogoutPage = PageinstancesFactory.getInstance(HerokuLogOutPage.class);
